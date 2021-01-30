60f193a481ddee53b847348372288f3b5b3e731c
Lance Hietpas <lancehietpas@gmail.com>
2021-01-30 07:56:43 -0600

    Update youtube.js

--- src/components/youtube.js
+++ src/components/youtube.js
@@ -1,1 +1,17 @@
+import React, { Component } from "react";
 
+export default class YouTube extends Component {
+  render() {
+    return (
+      <div className="youtube section" id="Youtube">
+        <div className="container">
+          <div className="section-head">
+            <h2 className="text-center"></h2>
+          </div>
+         <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+         <div class="elfsight-app-4e26020a-265c-4a56-a30b-c1fa72928406"></div>
+        </div>
+      </div>
+    );
+  }
+}
