d5b33708653bce0957f31766efd12fa864eec9c1
Lance Hietpas <lancehietpas@gmail.com>
2021-04-05 16:34:50 -0500

    Update service.js

--- src/components/service.js
+++ src/components/service.js
@@ -27,10 +27,9 @@ export default class service extends Component {
             })}
             
           </div>
-        </div>
         <script src="https://apps.elfsight.com/p/platform.js"></script>
-        <div class="elfsight-app-78dc7f60-06bf-4808-8bac-e470db8171ac">
-          </div>
+        <div class="elfsight-app-78dc7f60-06bf-4808-8bac-e470db8171ac"></div>
+        </div>
       </div>
     </div>
     );
