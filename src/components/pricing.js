e3961b5b447664dbbc008cb7155656e3ffb6db4a
HHPhotography <66795843+Mattsloniker@users.noreply.github.com>
2021-04-11 18:33:55 -0500

    Update pricing.js

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -4,6 +4,7 @@ export default class Pricing extends Component {
   render() {
     return (
     <div className="elfsight-app-78dc7f60-06bf-4808-8bac-e470db8171ac"></div>
+      <iframe srcdoc="<div class='elfsight-app-78dc7f60-06bf-4808-8bac-e470db8171ac'></div>"</iframe>
       );
   }
 }
