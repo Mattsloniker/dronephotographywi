6a6e03e1f6c7f3bfc3e1aafae573902f5f8621bc
Lance Hietpas <lancehietpas@gmail.com>
2021-04-11 16:33:26 -0500

    Update pricing.js

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,1 +1,10 @@
 import React, { Component } from "react";
+
+export default class pricing extends Component {
+  render() {
+    const { data } = this.props;
+    return (
+    <div class="elfsight-app-78dc7f60-06bf-4808-8bac-e470db8171ac"></div>
+      );
+  }
+}
