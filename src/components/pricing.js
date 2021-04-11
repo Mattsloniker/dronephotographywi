7e31338737b35229d078b33f44ac94de813ae038
Lance Hietpas <lancehietpas@gmail.com>
2021-04-11 17:32:52 -0500

    Update pricing.js

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -4,7 +4,7 @@ export default class Pricing extends Component {
   render() {
     return (
     <script src="https://apps.elfsight.com/p/platform.js" defer></script>
-    <div class="elfsight-app-78dc7f60-06bf-4808-8bac-e470db8171ac">a</div>
+    <div class="elfsight-app-78dc7f60-06bf-4808-8bac-e470db8171ac" />
       );
   }
 }
