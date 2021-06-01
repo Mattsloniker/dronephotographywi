aa4218492ac9556efaa70631770bb0d3692587c3
Mattsloniker <66795843+Mattsloniker@users.noreply.github.com>
2021-06-01 04:21:58 -0500

    update pricing table

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,9 +1,58 @@
-import React, { Component } from "react";
-
-export default class Pricing extends Component {
-  render() {
-    return (
-      <iframe src="https://apps.elfsight.com/widget/78dc7f60-06bf-4808-8bac-e470db8171ac"></iframe>
-      );
-  }
-}
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+<style>
+</style>
+</head>
+<body>
+<div class="column">
+  <ul class="price">
+    <li class="header">Silver Package</li> 
+    	<ul class="feature">
+        	<ul class="checkmark">
+            	<li>High Resolution Photos <br> (Pick your favorites)</br>
+                </li>
+            	<li>Professional Editing</li>
+            	<li>Commercial Licensing included</li>
+       </ul>
+          </ul>
+         	  	 <ul class="price">
+               		<li class="grey">$1,099</li>
+               		<li class="grey"><a href="https://calendly.com/hhphotography-1/silver-package?month=2021-06" class="button">Book Now</a></li>
+            </ul>
+  </ul>
+</div>
+<div class="column1"><div class="ribbon ribbon-top-right"><span>POPULAR</span></div>
+	<ul class="price">
+		<li class="header">Gold Package</li>
+			<ul class="feature1">
+				<ul class="checkmark1">
+					<li>4 Hours of Drone Photography</li>
+					<li>Professional Editing</li>
+                    <li>Commercial Licensing included</li>
+            </ul>
+                </ul>
+						<ul class="price">
+							<li class="grey1">$3,899</li>
+								<li class="grey1"><a href="https://calendly.com/hhphotography-1/gold-package?month=2021-06" class="button2">Book Now</a></li>
+            			</ul>   
+	</ul>
+</div>
+<div class="column2">
+	<ul class="price">
+		<li class="header">Diamond Package</li>
+			<ul class="feature2">
+				<ul class="checkmark">
+					<li>8 Hours of Drone Photography</li>
+					<li>Professional Editing</li>
+					<li>Commercial Licensing included</li>
+    		</ul>
+            	</ul>
+						<ul class="price">
+							<li class="grey">$7,399</li>
+								<li class="grey"><a href="https://calendly.com/hhphotography-1/diamond-package?month=2021-06" class="button">Book Now</a></li>
+						</ul>
+    </ul>
+</div>  
+</body>
+</html>
