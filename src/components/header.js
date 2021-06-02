c53f326c2e2263d1c0c9f51040bb48d739124d76
Mattsloniker <66795843+Mattsloniker@users.noreply.github.com>
2021-06-02 18:25:54 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -19,7 +19,7 @@ export default class Header extends Component {
             <div className="logo">
               <Link to="/">
                 {data.logo.file.url ? (
-                  <img src={data.logo.file.url} alt="logo" />
+                  <img src={data.logo.file.url} alt="logo"  width= "100%" height= "100%"/>
                 ) : (
                   <span>{data.siteName}</span>
                 )}
