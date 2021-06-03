d703da398d3982b7c9d4618d481b5fe6754de35e
Mattsloniker <66795843+Mattsloniker@users.noreply.github.com>
2021-06-02 23:56:04 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,8 +13,7 @@ export default class footer extends Component {
             <p class="text-justify">1131 Prairie Ave</p>
 <p class="text-justify">
 Beloit, WI 53511</p>
-<p class="text-justify">
-HHPhotography@dronephotographywi.com</p>
+<li><a href="HHPhotography@dronephotographywi.com">HHPhotography@dronephotographywi.com</a></li>
 <p class="text-justify">
 (608) 313-4495</p>
 <p class="text-justify">
@@ -41,18 +40,6 @@ Open 24/7</p>
             <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
          <a href="https://dronephotographywi.com/">HHPhotography</a>.
             </p>
-          </div>
-
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="https://www.facebook.com/HHPhotographyWI/"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="Youtube" href="https://www.youtube.com/channel/UCovZdjo6ztPtuBXGaIqTxdg"><i class="fa fa-youtube"></i></a></li>
-              <li><a class="twitter" href="https://twitter.com/hhphotographywi"><i class="fa fa-twitter"></i></a></li>
-              <li><a class="Snapchat" href="https://www.snapchat.com/add/wiphotographer/"><i class="fa fa-snapchat"></i></a></li>  
-              <li><a class="Instagram" href="https://www.instagram.com/hhphotographywi"><i class="fa fa-instagram"></i></a></li>   
-              <li><a class="linkedin" href="https://www.linkedin.com/public-profile/in/hhphotographywi"><i class="fa fa-linkedin"></i></a></li>  
-             <li><a class="Pinterest" href="https://www.pinterest.com/hhphotographywi/"><i class="fa fa-pinterest"></i></a></li>  
-            </ul>
           </div>
         </div>
       </div>
