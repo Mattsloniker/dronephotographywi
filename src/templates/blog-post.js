bd51b90742061fe17e2058ede1afb1313692ed97
Mattsloniker <66795843+Mattsloniker@users.noreply.github.com>
2021-06-03 15:53:07 -0500

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,6 +11,11 @@ import Share from "../components/share";
 export default class blogPost extends Component {
   render() {
     const data = this.props.data.contentfulBlogs;
+    const disqusShortname = "RohitGupta";
+    const disqusConfig = {
+      identifier: data.id,
+      title: data.title
+    };
 
     const siteurl = this.props.data.contentfulSiteInformation.siteUrl + "/";
     const twiteerhandle = this.props.data.contentfulSiteInformation
