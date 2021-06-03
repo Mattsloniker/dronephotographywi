6c4b212a89b60a6c3221f2dc23f2401bb7b27f6c
Mattsloniker <66795843+Mattsloniker@users.noreply.github.com>
2021-06-02 23:46:25 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,11 +3,57 @@ import React, { Component } from "react";
 export default class footer extends Component {
   render() {
     return (
+      <div>
       <div className="site-footer" id="footer">
         <div className="container">
-          <span>{this.props.siteName}</span>
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">1131 Prairie Ave</p>
+<p class="text-justify">
+Beloit, WI 53511</p>
+<p class="text-justify">
+(608) 313-4495</p>
+<p class="text-justify">
+Open 24/7</p>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <h6>Categories</h6>
+            <ul class="footer-links">
+              <li><a href="https://dronephotographywi.com">TOP</a></li>
+              <li><a href="https://dronephotographywi.com/about-us">About Us</a></li>
+              <li><a href="https://dronephotographywi.com/#Service">Pricing</a></li>
+              <li><a href="https://dronephotographywi.blog/">Blogs</a></li>
+              <li><a href="https://dronephotographywi.com/#Testimonials">Reviews</a></li>
+            </ul>
+          </div>
+          </div>
+            </div>
+          </div>
+        <hr>
+      </hr>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
+         <a href="https://dronephotographywi.com/">HHPhotography</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="https://www.facebook.com/HHPhotographyWI/"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="Youtube" href="https://www.youtube.com/channel/UCovZdjo6ztPtuBXGaIqTxdg"><i class="fa fa-youtube"></i></a></li>
+              <li><a class="twitter" href="https://twitter.com/hhphotographywi"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="Snapchat" href="https://www.snapchat.com/add/wiphotographer/"><i class="fa fa-snapchat"></i></a></li>  
+              <li><a class="Instagram" href="https://www.instagram.com/hhphotographywi"><i class="fa fa-instagram"></i></a></li>   
+              <li><a class="linkedin" href="https://www.linkedin.com/public-profile/in/hhphotographywi"><i class="fa fa-linkedin"></i></a></li>  
+             <li><a class="Pinterest" href="https://www.pinterest.com/hhphotographywi/"><i class="fa fa-pinterest"></i></a></li>  
+            </ul>
+          </div>
         </div>
       </div>
-    );
+      </div>
+    )
   }
 }
