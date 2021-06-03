fbd40e6de9e9d822c82e9147dfa3b32f6fe4cdac
Mattsloniker <66795843+Mattsloniker@users.noreply.github.com>
2021-06-03 00:30:19 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -39,6 +39,17 @@ export default class footer extends Component {
           </div>
         </div>
       </div>
+      <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="https://www.facebook.com/HHPhotographyWI/"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="Youtube" href="https://www.youtube.com/channel/UCovZdjo6ztPtuBXGaIqTxdg"><i class="fa fa-youtube"></i></a></li>
+              <li><a class="twitter" href="https://twitter.com/hhphotographywi"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="Snapchat" href="https://www.snapchat.com/add/wiphotographer/"><i class="fa fa-snapchat"></i></a></li>  
+              <li><a class="Instagram" href="https://www.instagram.com/hhphotographywi"><i class="fa fa-instagram"></i></a></li>   
+              <li><a class="linkedin" href="https://www.linkedin.com/public-profile/in/hhphotographywi"><i class="fa fa-linkedin"></i></a></li>  
+             <li><a class="Pinterest" href="https://www.pinterest.com/hhphotographywi/"><i class="fa fa-pinterest"></i></a></li>  
+            </ul>
+          </div>
       </div>
     )
   }
