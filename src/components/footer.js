4d3248faf17dcf04f4de042238192847ee80de1c
Mattsloniker <66795843+Mattsloniker@users.noreply.github.com>
2021-06-05 17:08:19 -0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -11,18 +11,18 @@ export default class footer extends Component {
             <h2>Contact</h2>
             <ul class="footer-links">
             <li><a href="https://g.page/HHPhotographyWI?share"><h3>1131 Prairie Ave, Beloit, WI 53511</h3></a></li>
-            <li><a href="mailto:HHPhotography@dronephotographywi.com"><h3>HHPhotography@dronephotographywi.com</h3></a></li>
-            <li><a href="tel:608-313-4495"><h3>(608) 313-4495</h3></a></li>
+            <li><a href="mailto:HHPhotography@dronephotographywi.com"><h3>Email us</h3></a></li>
+            <li><a href="tel:608-313-4495"><h3>Call or text us at (608) 313-4495</h3></a></li>
             </ul>
           </div>
           <div class="col-xs-6 col-md-3">
             <h2>Categories</h2>
             <ul class="footer-links">
-              <li><a href="https://dronephotographywi.com"><h3>Top</h3></a></li>
-              <li><a href="https://dronephotographywi.com/about-us"><h3>About</h3></a></li>
-              <li><a href="https://dronephotographywi.com/#Service"><h3>Pricing Table</h3></a></li>
-              <li><a href="https://dronephotographywi.blog/"><h3>Blog</h3></a></li>
-              <li><a href="https://dronephotographywi.com/#Testimonials"><h3>Testimonials</h3></a></li>
+              <li><a href="https://dronephotographywi.com"><h3>Top of the home page</h3></a></li>
+              <li><a href="https://dronephotographywi.com/about-us"><h3>Find more about us</h3></a></li>
+              <li><a href="https://dronephotographywi.com/#Service"><h3>Pricing table</h3></a></li>
+              <li><a href="https://dronephotographywi.blog/"><h3>Read our blogs</h3></a></li>
+              <li><a href="https://dronephotographywi.com/#Testimonials"><h3>What are customers have said</h3></a></li>
             </ul>
           </div>
           </div>
@@ -30,21 +30,21 @@ export default class footer extends Component {
           </div>
           <div class="col-md-4 col-sm-6 col-xs-12">
             <ul class="social-icons">
-              <li><a class="facebook" href="https://www.facebook.com/HHPhotographyWI/" aria-label="Facebook" target="_blank"
+              <li><a class="facebook" href="https://www.facebook.com/HHPhotographyWI/" aria-label="Follow us on Facebook" target="_blank"
                   rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a></li>
-              <li><a class="youtube" href="https://www.youtube.com/channel/UCovZdjo6ztPtuBXGaIqTxdg" aria-label="Youtube" target="_blank"
+              <li><a class="youtube" href="https://www.youtube.com/channel/UCovZdjo6ztPtuBXGaIqTxdg" aria-label="Follow Matt Sloniker on Youtube" target="_blank"
                   rel="noopener noreferrer"><i class="fab fa-youtube"></i></a></li>
-              <li><a class="twitter" href="https://twitter.com/hhphotographywi" aria-label="Twitter" target="_blank"
+              <li><a class="twitter" href="https://twitter.com/hhphotographywi" aria-label="Follow us on Twitter" target="_blank"
                   rel="noopener noreferrer"><i class="fab fa-twitter"></i></a></li>
-              <li><a class="snapchat" href="https://www.snapchat.com/add/wiphotographer/" aria-label="Snapchat" target="_blank"
+              <li><a class="snapchat" href="https://www.snapchat.com/add/wiphotographer/" aria-label="Follow Matt Sloniker on Snapchat" target="_blank"
                   rel="noopener noreferrer"><i class="fab fa-snapchat"></i></a></li>  
-              <li><a class="instagram" href="https://www.instagram.com/hhphotographywi" aria-label="Instagram" target="_blank"
+              <li><a class="instagram" href="https://www.instagram.com/hhphotographywi" aria-label="Follow us on Instagram" target="_blank"
                   rel="noopener noreferrer"><i class="fab fa-instagram"></i></a></li>   
-              <li><a class="linkedin" href="https://www.linkedin.com/public-profile/in/hhphotographywi" aria-label="Linkedin" target="_blank"
+              <li><a class="linkedin" href="https://www.linkedin.com/public-profile/in/hhphotographywi" aria-label="Follow Matt Sloniker on Linkedin" target="_blank"
                   rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a></li>  
-             <li><a class="tiktok" href="https://www.tiktok.com/@matt_sloniker" aria-label="Tiktok" target="_blank"
+             <li><a class="tiktok" href="https://www.tiktok.com/@matt_sloniker" aria-label="Follow Matt Sloniker on Tiktok" target="_blank"
                   rel="noopener noreferrer"><i class="fab fa-tiktok"></i></a></li>  
-             <li><a class="pinterest" href="https://www.pinterest.com/hhphotographywi/" aria-label="Pinterest" target="_blank"
+             <li><a class="pinterest" href="https://www.pinterest.com/hhphotographywi/" aria-label="Follow us on Pinterest" target="_blank"
                   rel="noopener noreferrer"><i class="fab fa-pinterest-p"></i></a></li>
             </ul>
           </div>
